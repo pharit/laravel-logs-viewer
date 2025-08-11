6821da78372bf2bbbb8da2fc1f23e547866d3cac
pharit <pharit.note@gmail.com>
2025-08-11 16:16:26 +0700

    fixed route

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Tong\LaravelLogsViewer\Http\Controllers\LogsViewerController;
+use Pharit\LaravelLogsViewer\Http\Controllers\LogsViewerController;
 
 Route::get('/', [LogsViewerController::class, 'index'])->name('logs-viewer.index');
 Route::get('/download', [LogsViewerController::class, 'download'])->name('logs-viewer.download');
