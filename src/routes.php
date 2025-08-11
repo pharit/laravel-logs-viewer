d43abf629bbf40ad353cba8043c17dfe35fd83a8
pharit <pharit.note@gmail.com>
2025-08-11 16:11:08 +0700

    update package name

--- src/routes.php
+++ src/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Iaa\LaravelLogsViewer\Http\Controllers\LogsViewerController;
+use Tong\LaravelLogsViewer\Http\Controllers\LogsViewerController;
 
 Route::get('/', [LogsViewerController::class, 'index'])->name('logs-viewer.index');
 Route::get('/download', [LogsViewerController::class, 'download'])->name('logs-viewer.download');
